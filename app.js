2029fd8e4243f4ca806690cc0305695a8e870217
Tanushree <122681103+tanu017@users.noreply.github.com>
2025-04-07 15:37:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { StudentData } = require("./src/controller/student_controller");
+const { EmailData } = require("./src/controller/Email_controller");
 const bodyparser = require("body-parser");
 const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 app.post("/studentinfo", StudentData);
+app.post("/send-email", EmailData);
 module.exports = app;
